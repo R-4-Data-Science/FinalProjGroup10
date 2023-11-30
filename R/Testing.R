9996e57678a595d1b4ec9f4635f6057c6420b070
KaosarUddin <muddin11@student.gsu.edu>
2023-11-30 09:52:30 -0600

    Add README FILE

--- R/Testing.R
+++ R/Testing.R
@@ -1,30 +1,8 @@
-set.seed(123)  # Setting seed for reproducibility
-n =200
-p = 2
 
-X = matrix(rnorm(n * p), ncol = p)
+#data = read.csv("F:/Fall 2023 AU/R Programming for Data Science/Practise/p1/expenses.csv")
 
-true_beta = c(1, -0.5)
-pi = 1 / (1 + exp(-X %*% true_beta))
-y = rbinom(n, 1, pi)
-
-beta_hat = logistic_regression(X, y)
-bootstrap_conf_intervals(X, y)
-plot_logistic_curve(X, beta_hat, y)
-confusion_matrix(X, y, beta_hat)
-Prevalence(X, y, beta_hat)
-Accuracy(X, y, beta_hat)
-Sensitivity(X, y, beta_hat)
-Specificity(X, y, beta_hat)
-False_Discovery_Rate(X, y, beta_hat)
-Diagnostic_Odds_ratio(X, y, beta_hat)
-prevalencegrid(X, y, beta_hat)
-
-
-data = read.csv("F:/Fall 2023 AU/R Programming for Data Science/Practise/p1/expenses.csv")
-
-#data = read.csv("expenses.csv")
-
+data = read.csv("expenses.csv")
+library("p1")
 # Prepare the predictor matrix X (using 'bmi' as the predictor)
 X = matrix(data$bmi, ncol = 1)
 X = scale(X)  # Standardizing X
